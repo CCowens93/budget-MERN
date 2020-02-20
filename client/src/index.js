7a1cf2f25dfb59a5d5eb71f5e19d914e6e6309c4
Cassandra Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-02-20 13:02:32 -0500

    database debugging

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import { ReactDOM, render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-// Apollo
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
-import { setContext } from "apollo-link-context";
-import { ApolloProvider } from "@apollo/react-hooks";
-// Stitch
-import { Stitch, AnonymousCredential } from "mongodb-stitch-browser-sdk";
-// Check out app.js for examples of how to run GraphQL operations
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -19,62 +11,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-// React
-
-
-
-
-// To set up your app:
-//
-// 1. Link a cluster that includes the Atlas sample data sets
-// 2. Configure permissions for the ``sample_mflix.movies`` collection. For this
-//    demo, you can assign full read/write permissions to the default role.
-// 3. Generate a collection schema for the ``sample_mflix.movies`` collection.
-//    Add a root-level `title` field to the schema with the value set to "Movie".
-// 4. Enable anonymous authentication
-// 5. Deploy your changes
-//
-// Once your app is set up, replace the value of APP_ID with your :guilabel:`App ID`
-export const APP_ID = "mern-budget";
-
-// Instantiate a StitchClient
-const app = Stitch.hasAppClient(APP_ID)
-  ? Stitch.getAppClient(APP_ID)
-  : Stitch.initializeAppClient(APP_ID);
-
-// Get the logged in user's access token from the StitchAuth interface
-export async function getAccessToken(credential) {
-  await app.auth.loginWithCredential(credential);
-  const { accessToken } = app.auth.activeUserAuthInfo;
-  return accessToken;
-}
-
-// Add an Authorization header with a valid user access token to all requests
-const credential = new AnonymousCredential();
-const authorizationHeaderLink = setContext(async (_, { headers }) => {
-  const accessToken = await getAccessToken(credential);
-  return {
-    headers: {
-      ...headers,
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
-});
-
-// Construct a new Apollo HttpLink that connects to your app's GraphQL endpoint
-const graphql_url = `https://stitch.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`;
-const httpLink = new HttpLink({ uri: graphql_url });
-
-// Construct a new Apollo client with the links we just defined
-const client = new ApolloClient({
-  link: authorizationHeaderLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-render(
-  // Wrap your app with an ApolloProvider
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root"),
-);
